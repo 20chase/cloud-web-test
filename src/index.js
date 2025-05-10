e7e02a919cea8d452be07cd89bdee54abc2da770
tingxfan <tingxiang@lightrobo.com>
2025-05-10 21:07:31 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+export default {
+    async fetch(request, env, ctx) {
+      const url = new URL(request.url);
+  
+      const target = "http://imz410.ust.hk:1180" + url.pathname + url.search;
+  
+      const modifiedRequest = new Request(target, {
+        method: request.method,
+        headers: request.headers,
+        body: request.body,
+        redirect: "follow"
+      });
+  
+      return fetch(modifiedRequest);
+    }
+  }
