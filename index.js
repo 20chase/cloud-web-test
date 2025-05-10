365acb376e6c3fed0db7f8c87ef78ccb1f96c7a4
tingxfan <tingxiang@lightrobo.com>
2025-05-10 22:01:17 +0800

    test proxy

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 export default {
-  async fetch(request, env, ctx) {
+  async fetch(request) {
     const url = new URL(request.url);
     const target = "http://imz410.ust.hk:1180" + url.pathname + url.search;
 
-    const modifiedRequest = new Request(target, {
+    const proxyRequest = new Request(target, {
       method: request.method,
       headers: request.headers,
       body: request.body,
-      redirect: "follow"
+      redirect: 'manual'
     });
 
-    return fetch(modifiedRequest);
+    const response = await fetch(proxyRequest);
+
+    // 复制响应（包括 401 challenge）
+    return new Response(response.body, {
+      status: response.status,
+      statusText: response.statusText,
+      headers: response.headers
+    });
   }
 }
